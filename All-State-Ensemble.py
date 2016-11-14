b3688e57ff59a7ad956e055f920bcc4a6c0b60f8
mrbraveheart71 <mrbraveheart71@gmail.com>
2016-11-13 20:25:41 -0500

    Keras

--- All-State-Ensemble.py
+++ All-State-Ensemble.py
@@ -5,7 +5,7 @@ import scipy.stats as sc
 df_Keras = pd.read_csv('submission_keras_shift_perm.csv')
 df_Xgboost = pd.read_csv('sub_v_5_long2_python.csv')
 
-df = pd.DataFrame({'id': df_Keras['id'], 'loss': df_Keras['loss']*0.4+df_Xgboost['loss']*0.6})
+df = pd.DataFrame({'id': df_Keras['id'], 'loss': df_Keras['loss']*0.5+df_Xgboost['loss']*0.5})
 df.to_csv('Ensemble All State.csv', index = False)
 
 #n, bins, patches = plt.hist(df_Keras['loss'], 50, normed=1, facecolor='green', alpha=0.75)
